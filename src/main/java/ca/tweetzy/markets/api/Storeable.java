76f6dbc312de0c83dccaf7018a982cff0d9eb6a7
Kiran Hart <kiran.hart@live.com>
2023-01-05 20:45:10 -0500

    ❌ Added default unstore to Storeable interface
    Took 42 seconds

--- src/main/java/ca/tweetzy/markets/api/Storeable.java
+++ src/main/java/ca/tweetzy/markets/api/Storeable.java
@@ -1,10 +1,16 @@
 package ca.tweetzy.markets.api;
 
 import lombok.NonNull;
+import org.jetbrains.annotations.Nullable;
 
 import java.util.function.Consumer;
 
 public interface Storeable<T> {
 
 	void store(@NonNull final Consumer<T> stored);
+
+	default void unStore(@Nullable final Consumer<SynchronizeResult> syncResult) {
+		if (syncResult != null)
+			syncResult.accept(SynchronizeResult.SUCCESS);
+	}
 }
