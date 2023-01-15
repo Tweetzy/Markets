ec3323b0e850c6d35a0c69f1e624e0d91d30dec5
Kiran Hart <kiran.hart@live.com>
2023-01-14 20:46:43 -0500

    🪙 Add the vault currency implementation
    Took 39 minutes

---
+++ src/main/java/ca/tweetzy/markets/impl/currency/VaultCurrency.java
@@ -0,0 +1,27 @@
+package ca.tweetzy.markets.impl.currency;
+
+import ca.tweetzy.markets.Markets;
+import ca.tweetzy.markets.api.currency.AbstractCurrency;
+import org.bukkit.OfflinePlayer;
+
+public final class VaultCurrency extends AbstractCurrency {
+
+	public VaultCurrency() {
+		super("Vault", "Vault");
+	}
+
+	@Override
+	public boolean has(OfflinePlayer player, double amount) {
+		return Markets.getEconomy().has(player, amount);
+	}
+
+	@Override
+	public boolean withdraw(OfflinePlayer player, double amount) {
+		return Markets.getEconomy().withdrawPlayer(player, amount).transactionSuccess();
+	}
+
+	@Override
+	public boolean deposit(OfflinePlayer player, double amount) {
+		return Markets.getEconomy().depositPlayer(player, amount).transactionSuccess();
+	}
+}
