b4bc34469e586b6960946ac27046f2c1a05a3882
Kiran Hart <kiran.hart@live.com>
2023-01-24 22:12:27 -0500

    ➕ translate for left_click variable, and use inside border fill of 3 rows
    Took 6 minutes

---
+++ src/main/java/ca/tweetzy/markets/api/market/LayoutControl.java
@@ -0,0 +1,11 @@
+package ca.tweetzy.markets.api.market;
+
+public enum LayoutControl {
+
+	EXIT_BACK_BUTTON,
+	PROFILE_BUTTON,
+	PREVIOUS_PAGE_BUTTON,
+	NEXT_PAGE_BUTTON,
+	SEARCH_BUTTON,
+	REVIEW_BUTTON
+}
--- src/main/java/ca/tweetzy/markets/gui/user/market/LayoutControlPicker.java
+++ src/main/java/ca/tweetzy/markets/gui/user/market/LayoutControlPicker.java
@@ -1,8 +1,8 @@
 package ca.tweetzy.markets.gui.user.market;
 
-import ca.tweetzy.flight.comp.enums.CompMaterial;
 import ca.tweetzy.flight.gui.Gui;
 import ca.tweetzy.flight.gui.events.GuiClickEvent;
+import ca.tweetzy.flight.gui.helper.InventoryBorder;
 import ca.tweetzy.flight.gui.template.PagedGUI;
 import ca.tweetzy.flight.settings.TranslationManager;
 import ca.tweetzy.flight.utils.QuickItem;
@@ -14,6 +14,7 @@ import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 
 import java.util.Arrays;
+import java.util.List;
 import java.util.function.Consumer;
 
 public final class LayoutControlPicker extends PagedGUI<LayoutControl> {
@@ -34,36 +35,36 @@ public final class LayoutControlPicker extends PagedGUI<LayoutControl> {
 			case EXIT_BACK_BUTTON -> QuickItem
 					.of(Settings.GUI_LAYOUT_CONTROL_PICKER_ITEMS_EXIT.getItemStack())
 					.name(TranslationManager.string(this.player, Translations.GUI_LAYOUT_CONTROL_PICKER_ITEMS_EXIT_NAME))
-					.lore(TranslationManager.list(this.player, Translations.GUI_LAYOUT_CONTROL_PICKER_ITEMS_EXIT_LORE))
+					.lore(TranslationManager.list(this.player, Translations.GUI_LAYOUT_CONTROL_PICKER_ITEMS_EXIT_LORE, "left_click", TranslationManager.string(this.player, Translations.MOUSE_LEFT_CLICK)))
 					.make();
 
 			case PROFILE_BUTTON -> QuickItem
 					.of(this.player)
 					.name(TranslationManager.string(this.player, Translations.GUI_LAYOUT_CONTROL_PICKER_ITEMS_PROFILE_NAME))
-					.lore(TranslationManager.list(this.player, Translations.GUI_LAYOUT_CONTROL_PICKER_ITEMS_PROFILE_LORE))
+					.lore(TranslationManager.list(this.player, Translations.GUI_LAYOUT_CONTROL_PICKER_ITEMS_PROFILE_LORE, "left_click", TranslationManager.string(this.player, Translations.MOUSE_LEFT_CLICK)))
 					.make();
 
 			case PREVIOUS_PAGE_BUTTON -> QuickItem
 					.of(Settings.GUI_LAYOUT_CONTROL_PICKER_ITEMS_PREV_PAGE.getItemStack())
 					.name(TranslationManager.string(this.player, Translations.GUI_LAYOUT_CONTROL_PICKER_ITEMS_BACK_NAME))
-					.lore(TranslationManager.list(this.player, Translations.GUI_LAYOUT_CONTROL_PICKER_ITEMS_BACK_LORE))
+					.lore(TranslationManager.list(this.player, Translations.GUI_LAYOUT_CONTROL_PICKER_ITEMS_BACK_LORE, "left_click", TranslationManager.string(this.player, Translations.MOUSE_LEFT_CLICK)))
 					.make();
 			case NEXT_PAGE_BUTTON -> QuickItem
 					.of(Settings.GUI_LAYOUT_CONTROL_PICKER_ITEMS_NEXT_PAGE.getItemStack())
 					.name(TranslationManager.string(this.player, Translations.GUI_LAYOUT_CONTROL_PICKER_ITEMS_NEXT_NAME))
-					.lore(TranslationManager.list(this.player, Translations.GUI_LAYOUT_CONTROL_PICKER_ITEMS_NEXT_LORE))
+					.lore(TranslationManager.list(this.player, Translations.GUI_LAYOUT_CONTROL_PICKER_ITEMS_NEXT_LORE, "left_click", TranslationManager.string(this.player, Translations.MOUSE_LEFT_CLICK)))
 					.make();
 
 			case SEARCH_BUTTON -> QuickItem
 					.of(Settings.GUI_LAYOUT_CONTROL_PICKER_ITEMS_SEARCH.getItemStack())
 					.name(TranslationManager.string(this.player, Translations.GUI_LAYOUT_CONTROL_PICKER_ITEMS_SEARCH_NAME))
-					.lore(TranslationManager.list(this.player, Translations.GUI_LAYOUT_CONTROL_PICKER_ITEMS_SEARCH_LORE))
+					.lore(TranslationManager.list(this.player, Translations.GUI_LAYOUT_CONTROL_PICKER_ITEMS_SEARCH_LORE, "left_click", TranslationManager.string(this.player, Translations.MOUSE_LEFT_CLICK)))
 					.make();
 
 			case REVIEW_BUTTON -> QuickItem
 					.of(Settings.GUI_LAYOUT_CONTROL_PICKER_ITEMS_REVIEW.getItemStack())
 					.name(TranslationManager.string(this.player, Translations.GUI_LAYOUT_CONTROL_PICKER_ITEMS_REVIEW_NAME))
-					.lore(TranslationManager.list(this.player, Translations.GUI_LAYOUT_CONTROL_PICKER_ITEMS_REVIEW_LORE))
+					.lore(TranslationManager.list(this.player, Translations.GUI_LAYOUT_CONTROL_PICKER_ITEMS_REVIEW_LORE, "left_click", TranslationManager.string(this.player, Translations.MOUSE_LEFT_CLICK)))
 					.make();
 		};
 	}
@@ -71,5 +72,10 @@ public final class LayoutControlPicker extends PagedGUI<LayoutControl> {
 	@Override
 	protected void onClick(LayoutControl layoutControl, GuiClickEvent click) {
 		this.selectedControl.accept(layoutControl);
+	}
+
+	@Override
+	protected List<Integer> fillSlots() {
+		return InventoryBorder.getInsideBorders(3);
 	}
 }
