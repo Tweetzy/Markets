7a95b1f2492ec99766c13dba44c46c70edbee8d4
Kiran Hart <kiran.hart@live.com>
2023-02-06 15:28:44 -0500

    📦 Importer abstract & v1 class
    Took 4 minutes

---
+++ src/main/java/ca/tweetzy/markets/model/importer/MarketsV1Importer.java
@@ -0,0 +1,19 @@
+package ca.tweetzy.markets.model.importer;
+
+import lombok.NonNull;
+
+import java.util.function.Consumer;
+
+public final class MarketsV1Importer extends Importer{
+
+	public MarketsV1Importer() {
+		super("Markets v1");
+	}
+
+	@Override
+	public void run(@NonNull Consumer<Boolean> success) {
+
+
+
+	}
+}
