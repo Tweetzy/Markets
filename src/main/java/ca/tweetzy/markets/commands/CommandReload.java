a84a96295845525128f452476e42b0bec88f3275
Kiran Hart <kiran.hart@live.com>
2023-05-28 18:18:42 -0400

    ➕ add reload command
    Took 25 minutes

---
+++ src/main/java/ca/tweetzy/markets/commands/CommandReload.java
@@ -0,0 +1,49 @@
+package ca.tweetzy.markets.commands;
+
+import ca.tweetzy.flight.command.AllowedExecutor;
+import ca.tweetzy.flight.command.Command;
+import ca.tweetzy.flight.command.ReturnType;
+import ca.tweetzy.flight.utils.Common;
+import ca.tweetzy.markets.Markets;
+import ca.tweetzy.markets.gui.user.BankGUI;
+import ca.tweetzy.markets.settings.Settings;
+import ca.tweetzy.markets.settings.Translations;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+import java.util.List;
+
+public final class CommandReload extends Command {
+
+	public CommandReload() {
+		super(AllowedExecutor.BOTH, "reload");
+	}
+
+	@Override
+	protected ReturnType execute(CommandSender sender, String... args) {
+		Settings.init();
+		Translations.init();
+		Common.setPrefix(Settings.PREFIX.getStringOr("&8[&EMarkets&8]"));
+		return ReturnType.SUCCESS;
+	}
+
+	@Override
+	protected List<String> tab(CommandSender sender, String... args) {
+		return null;
+	}
+
+	@Override
+	public String getPermissionNode() {
+		return "markets.command.reload";
+	}
+
+	@Override
+	public String getSyntax() {
+		return "reload";
+	}
+
+	@Override
+	public String getDescription() {
+		return "Reloads all the configuration files";
+	}
+}
